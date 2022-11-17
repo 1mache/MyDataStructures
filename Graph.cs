8ea7ced86bfcafda1c7de598b2ffd929286e1639
Imache <miktor4925@gmail.com>
2022-11-17 02:27:54 +0200

    Implemented Indirected Unweighted graph class with ability to create/ remove vertecies/edges

---
+++ Graph.cs
@@ -0,0 +1,101 @@
+namespace MyDataStructures
+{
+    //undirected unweighted
+    class Graph<T>
+    {
+        private HashTable<T,DynArray<T>> _adjacencyList;
+
+        public Graph()
+        {
+            _adjacencyList = new HashTable<T, DynArray<T>>();
+        }
+
+        public void AddVertex(T vertex)
+        {
+            if(vertex is null)
+                throw new ArgumentNullException("Null vertex");
+            
+            try
+            {
+                _adjacencyList.Add(vertex, new DynArray<T>());
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException("Vertex already in Graph");
+            }
+        }
+
+        public void AddEdge(T vertex1, T vertex2)
+        {
+            if(vertex1 is null || vertex2 is null )
+                throw new ArgumentNullException("Null vertex");
+            
+            if(vertex1.Equals(vertex2))
+                throw new InvalidOperationException("Cannot create edge from vertex to itself");
+
+            if(!_adjacencyList.Contains(vertex1))
+                throw new InvalidOperationException("Vertex is not in Graph");
+            if(!_adjacencyList.Contains(vertex2))
+                throw new InvalidOperationException("Vertex is not in Graph");
+
+            if(_adjacencyList[vertex1].Contains(vertex2))
+                throw new InvalidOperationException("Edge already exists");
+
+            _adjacencyList[vertex1].Add(vertex2);
+            _adjacencyList[vertex2].Add(vertex1);
+        }
+
+        public void RemoveEdge(T vertex1, T vertex2)
+        {
+            if(vertex1 is null || vertex2 is null )
+                throw new ArgumentNullException("Null vertex");
+            
+            if(vertex1.Equals(vertex2))
+                throw new InvalidOperationException("Cannot remove edge from vertex to itself");
+            
+            if(!_adjacencyList.Contains(vertex1))
+                throw new InvalidOperationException("Vertex is not in Graph");
+            if(!_adjacencyList.Contains(vertex2))
+                throw new InvalidOperationException("Vertex is not in Graph");
+
+
+            _adjacencyList[vertex1].Remove(vertex2);
+            bool removed = _adjacencyList[vertex2].Remove(vertex1);
+            if (!removed) 
+                throw new InvalidOperationException("Edge does not exist");
+        }
+
+        public void RemoveVertex(T vertex)
+        {
+            if(vertex is null)
+                throw new ArgumentNullException("Null vertex");
+
+            if(!_adjacencyList.Contains(vertex))
+                throw new InvalidOperationException("Vertex is not in Graph");
+
+            while(_adjacencyList[vertex].Length > 0)
+            {
+                //take first elemnt at given vertex's list of connections
+                var other = _adjacencyList[vertex][0];
+                //removeEdge will remove the vertex from this list so 
+                //we can take the first one each iteration and it will be a different connection.
+                RemoveEdge(vertex, other!);
+            }
+
+            _adjacencyList.Remove(vertex);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            foreach (var key in _adjacencyList.Keys())
+            {
+                result += $"({key!.ToString()})";
+                result += $" => {_adjacencyList[key].ToString()}";
+                result += "\n";    
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,21 +13,32 @@
         }
         static void Main(string[] args)
         {
-            var ht = new HashTable<string, int>();
-            ht.Add("Dima", 19);
-            ht.Add("Dimon", 29);
-            ht.Add("Dlinnopop", 1);
-            ht.Add("Mama", 45);
-            ht.Add("Dobrre", 15);
-            ht.Add("Vovma", 9);
-            ht.Add("Bobaa", 99);
-            ht.Add("Lalo", 89);
-            ht.Add("Lily", 9);
-            ht.Remove("Mama");
-            System.Console.WriteLine(ht["Lalo"]);
-            System.Console.WriteLine(ht.Contains("Mama"));
-            ht.Keys();
-            ht.Values();
+            // var ht = new HashTable<string, int>();
+            // ht.Add("Dima", 19);
+            // ht.Add("Dimon", 29);
+            // ht.Add("Dlinnopop", 1);
+            // ht.Add("Mama", 45);
+            // ht.Add("Dobrre", 15);
+            // ht.Add("Vovma", 9);
+            // ht.Add("Bobaa", 99);
+            // ht.Add("Lalo", 89);
+            // ht.Add("Lily", 9);
+            // ht.Remove("Mama");
+            // System.Console.WriteLine(ht["Lalo"]);
+            // System.Console.WriteLine(ht.Contains("Mama"));
+            // ht.Keys();
+            // ht.Values();
+
+            var graph = new Graph<string>();
+            graph.AddVertex("Ohio");
+            graph.AddVertex("North Carolina");
+            graph.AddVertex("NY");
+            graph.AddVertex("Florida");
+            graph.AddEdge("NY", "Ohio");
+            graph.AddEdge("Ohio", "Florida");
+            graph.RemoveVertex("NY");
+            System.Console.WriteLine(graph.ToString());
+
         }
     }  
 }
